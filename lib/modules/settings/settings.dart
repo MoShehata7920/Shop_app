2c183ca4bc832b2e8b084a1b6b6dc1f05def15ec
Mohamed Shehata <100875413+MoShehata7920@users.noreply.github.com>
2022-09-23 03:21:51 +0200

    created profile in settings screen which will show ur data

--- lib/modules/settings/settings.dart
+++ lib/modules/settings/settings.dart
@@ -1,18 +1,82 @@
+import 'package:conditional_builder_null_safety/conditional_builder_null_safety.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shop_app/layout/cubit/cubit.dart';
+import 'package:shop_app/layout/cubit/states.dart';
+import 'package:shop_app/shared/component/component.dart';
 
 class SettingsScreen extends StatelessWidget {
-  const SettingsScreen({Key? key}) : super(key: key);
+  SettingsScreen({Key? key}) : super(key: key);
+
+  var nameController = TextEditingController();
+  var emailController = TextEditingController();
+  var phoneController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'setting Screen',
-        style: Theme.of(context).textTheme.bodyText1,
-      ),
+    return BlocConsumer<ShopCubit, ShopStates>(
+      listener: (context, state) {},
+      builder: (context, state) {
+        var model = ShopCubit.get(context).userModel;
+
+        nameController.text = model!.data!.name!;
+        emailController.text = model.data!.email!;
+        phoneController.text = model.data!.phone!;
+
+        return ConditionalBuilder(
+          condition: ShopCubit.get(context).userModel != null,
+          builder: (context) => Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              children: [
+                defaultFormField(
+                    controller: nameController,
+                    type: TextInputType.name,
+                    validate: (String? value) {
+                      if (value!.isEmpty) {
+                        return 'name must not be empty';
+                      }
+                      return null;
+                    },
+                    label: 'Name',
+                    prefix: Icons.person),
+                const SizedBox(
+                  height: 20,
+                ),
+                defaultFormField(
+                    controller: emailController,
+                    type: TextInputType.emailAddress,
+                    validate: (String? value) {
+                      if (value!.isEmpty) {
+                        return 'email must not be empty';
+                      }
+                      return null;
+                    },
+                    label: 'Email Address',
+                    prefix: Icons.email_outlined),
+                const SizedBox(
+                  height: 20,
+                ),
+                defaultFormField(
+                    controller: phoneController,
+                    type: TextInputType.phone,
+                    validate: (String? value) {
+                      if (value!.isEmpty) {
+                        return 'phone must not be empty';
+                      }
+                      return null;
+                    },
+                    label: 'Phone',
+                    prefix: Icons.phone),
+              ],
+            ),
+          ),
+          fallback: (context) => const CircularProgressIndicator(),
+        );
+      },
     );
   }
 }
